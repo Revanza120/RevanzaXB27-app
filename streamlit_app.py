eb007c985ade07b1af7a0488cc991c3334f4b8dd
Revanza120 <revanzafawaz@gmail.com>
2025-06-02 16:46:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("Belajar membuat web bersama sama")
 st.write(
     "ft. Lil bud, Cuan, Woyo, Yidyud, Wanrizz, Raizal, my man eman, and the one and only deznat"
 )
-st.image"ngafalin fisika.jpg", width=100
+st.(image"ngafalin fisika.jpg", width=300)
